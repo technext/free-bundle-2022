73931d121fc4712b980f64b8362607f7eb066291
lk <liakot.babu98@gmail.com>
2022-03-21 15:05:14 +0600

    issue fixed

--- src/js/isotope.js
+++ src/js/isotope.js
@@ -1,35 +1,60 @@
 /*eslint-disable*/
 
 import utils from './utils';
+/*-----------------------------------------------
+|                     Isotope
+-----------------------------------------------*/
 
 const isotopeFilter = () => {
-	var iso = new Isotope('.grid', {
-		itemSelector: '.item',
-		layoutMode: 'packery',
-		masonry: {
-			// use element for option
-			columnWidth: '.item'
-		}
-	});
+	const Selector = {
+		ISOTOPE_ITEM: '.isotope-item',
+		DATA_ISOTOPE: '[data-isotope]',
+		DATA_FILTER: '[data-filter]',
+		DATA_FILER_NAV: '[data-bs-nav]'
+	};
 
-	var filtersElem = document.querySelectorAll('[data-bs-nav]');
+	const DATA_KEY = {
+		ISOTOPE: 'isotope'
+	};
+	const ClassName = {
+		ACTIVE: 'active'
+	};
 
-	filtersElem.forEach(element => {
-		element.addEventListener('click', function (event) {
-			var filterValue = event.target.getAttribute('data-filter');
+	if (window.Isotope) {
+		const masonryItems = document.querySelectorAll(Selector.DATA_ISOTOPE);
+		masonryItems.length &&
+			masonryItems.forEach(masonryItem => {
+				window.imagesLoaded(masonryItem, () => {
+					console.log('running');
+					masonryItem.querySelectorAll(Selector.ISOTOPE_ITEM).forEach(item => {
+						// eslint-disable-next-line
+						item.style.visibility = 'visible';
+					});
 
-			iso.arrange({ filter: filterValue });
-		});
-	});
+					const defaultOptions = {
+						itemSelector: Selector.ISOTOPE_ITEM,
+						layoutMode: 'packery'
+					};
+					// const options = window._.merge(defaultOptions, userOptions);
+					const isotope = new window.Isotope(masonryItem, defaultOptions);
 
-	var dropDownEl = document.querySelectorAll('.dropdown-item');
-	dropDownEl.forEach(element => {
-		element.addEventListener('click', function (event) {
-			var filterValue = event.target.getAttribute('data-filter');
+					//--------- filter -----------------
+					let filtersElem = document.querySelectorAll('[data-bs-nav]');
 
-			iso.arrange({ filter: filterValue });
-		});
-	});
+					filtersElem.forEach(element => {
+						element.addEventListener('click', function (event) {
+							let filterValue = event.target.getAttribute('data-filter');
+
+							isotope.arrange({ filter: filterValue });
+						});
+					});
+
+					//---------- filter end ------------
+
+					return isotope;
+				});
+			});
+	}
 };
 
 export default isotopeFilter;
