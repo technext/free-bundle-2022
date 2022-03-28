6c82334a0a0fb080964d2b2b612f34392a2d7cf7
lk <liakot.babu98@gmail.com>
2022-03-28 15:11:08 +0600

    issue fixed

--- src/js/isotope.js
+++ src/js/isotope.js
@@ -17,43 +17,43 @@ const isotopeFilter = () => {
 		const masonryItems = document.querySelectorAll(Selector.DATA_ISOTOPE);
 		masonryItems.length &&
 			masonryItems.forEach(masonryItem => {
-				// window.imagesLoaded(masonryItem, () => {
-				// 	masonryItem.querySelectorAll(Selector.ISOTOPE_ITEM).forEach(item => {
-				// 		// eslint-disable-next-line
-				// 		item.style.visibility = 'visible';
-				// 	});
+				window.imagesLoaded(masonryItem, () => {
+					// masonryItem.querySelectorAll(Selector.ISOTOPE_ITEM).forEach(item => {
+					// 	// eslint-disable-next-line
+					// 	item.style.visibility = 'visible';
+					// });
 
-				const defaultOptions = {
-					itemSelector: Selector.ISOTOPE_ITEM,
-					layoutMode: 'packery'
-				};
-				// const options = window._.merge(defaultOptions, userOptions);
-				const isotope = new window.Isotope(masonryItem, defaultOptions);
+					const defaultOptions = {
+						itemSelector: Selector.ISOTOPE_ITEM,
+						layoutMode: 'packery'
+					};
+					// const options = window._.merge(defaultOptions, userOptions);
+					const isotope = new window.Isotope(masonryItem, defaultOptions);
 
-				//--------- filter -----------------
-				let filtersElem = document.querySelectorAll('[data-bs-nav]');
+					//--------- filter -----------------
+					let filtersElem = document.querySelectorAll('[data-bs-nav]');
 
-				filtersElem.forEach(element => {
-					element.addEventListener('click', function (event) {
-						let filterValue = event.target.getAttribute('data-filter');
+					filtersElem.forEach(element => {
+						element.addEventListener('click', function (event) {
+							let filterValue = event.target.getAttribute('data-filter');
 
-						isotope.arrange({ filter: filterValue });
+							isotope.arrange({ filter: filterValue });
+						});
 					});
-				});
 
-				let dropDownEl = document.querySelectorAll('.dropdown-item');
-				dropDownEl.forEach(element => {
-					element.addEventListener('click', function (event) {
-						let filterValue = event.target.getAttribute('data-filter');
+					let dropDownEl = document.querySelectorAll('.dropdown-item');
+					dropDownEl.forEach(element => {
+						element.addEventListener('click', function (event) {
+							let filterValue = event.target.getAttribute('data-filter');
 
-						isotope.arrange({ filter: filterValue });
+							isotope.arrange({ filter: filterValue });
+						});
 					});
+
+					//---------- filter end ------------
+
+					return isotope;
 				});
-
-				//---------- filter end ------------
-
-				return isotope;
-				// });
 			});
 	}
 };
