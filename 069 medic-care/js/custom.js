278a680e4bc9fe836836a29957257bf9d8026795
lk <liakot.babu98@gmail.com>
2022-04-05 11:31:54 +0600

    issue fixed

---
+++ 069 medic-care/js/custom.js
@@ -0,0 +1,68 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    // REVIEWS CAROUSEL
+    $('.reviews-carousel').owlCarousel({
+        center: true,
+        loop: true,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 300,
+        smartSpeed: 500,
+        responsive:{
+          0:{
+            items:1,
+          },
+          768:{
+            items:2,
+            margin: 100,
+          },
+          1280:{
+            items:2,
+            margin: 100,
+          }
+        }
+    });
+
+    // Banner Carousel
+    var myCarousel = document.querySelector('#myCarousel')
+    var carousel = new bootstrap.Carousel(myCarousel, {
+      interval: 1500,
+    })
+
+    // REVIEWS NAVIGATION
+    function ReviewsNavResize(){
+      $(".navbar").scrollspy({ offset: -94 });
+
+      var ReviewsOwlItem = $('.reviews-carousel .owl-item').width();
+
+      $('.reviews-carousel .owl-nav').css({'width' : (ReviewsOwlItem) + 'px'});
+    }
+
+    $(window).on("resize", ReviewsNavResize);
+    $(document).on("ready", ReviewsNavResize);
+
+    // HREF LINKS
+    $('a[href*="#"]').click(function (event) {
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top - 74
+          }, 1000);
+        }
+      }
+    });
+    
+  })(window.jQuery);
